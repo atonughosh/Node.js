f6dd7ea6de563220886ca40efe08e0a62f95a628
ATONU GHOSH <atonughosh@outlook.com>
2017-10-06 22:41:44 +0530

    Update 7. objects.js

--- Practice Codes/7. objects.js
+++ Practice Codes/7. objects.js
@@ -1,1 +1,10 @@
+var pet = {
+	'name' : 'Tommy',
+	'type' : 'Dog'
+};
 
+function printPet(yourPet) {
+	console.log('You own a ' + yourPet.type + " that's named " + yourPet.name + '.');
+}
+
+printPet(pet);
