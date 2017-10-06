b6cb93b3239bbe71d9724518e2b227bcba058ed3
ATONU GHOSH <atonughosh@outlook.com>
2017-10-06 22:41:06 +0530

    Update 5. if.js

--- Practice Codes/5. if.js
+++ Practice Codes/5. if.js
@@ -1,1 +1,9 @@
+var friend = 'Ram';
 
+if (friend === 'Sumit' || friend === 'Arvind' || friend === 'Adil'){
+	console.log('Hello friend!');
+} else if (friend === 'Mohit' || friend === 'Koustav'){
+	console.log('Hey!');
+} else {
+	console.log('Hello stranger!');
+}
