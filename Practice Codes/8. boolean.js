f046552270633c8fc0e3ea5882eb81ebb41e670e
ATONU GHOSH <atonughosh@outlook.com>
2017-10-07 23:22:48 +0530

    Delete 8. boolean.js

--- Practice Codes/8. boolean.js
+++
@@ -1,9 +0,0 @@
-//function to flip only boolean variable
-function toggleBoolean (variable) {
-	if (typeof variable === 'boolean') {
-		return !variable;
-	}
-}
-
-
-console.log(toggleBoolean(true));
